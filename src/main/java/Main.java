01ab225163aa31fc52dceb6abe95ff9f2819870f
asungkim <asungkim007@naver.com>
2025-01-10 23:43:23 +0900

    level5 done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // 명언 앱 만들기 20분
         Scanner sc = new Scanner(System.in);
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
         System.out.println("== 명언 앱 ==");
-        int cnt=1;
+        int cnt = 1;
         while (true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine().trim();
@@ -17,7 +20,19 @@ public class Main {
                 String wiseSaying = sc.nextLine().trim();
                 System.out.print("작가 : ");
                 String author = sc.nextLine().trim();
+
+                wiseSayings.add(new WiseSaying(cnt, wiseSaying, author));
                 System.out.println("%d번 명언이 등록되었습니다".formatted(cnt++));
+
+
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                    WiseSaying wiseSaying = wiseSayings.get(i);
+                    System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getContent(), wiseSaying.getAuthor()));
+                }
             }
         }
     }
